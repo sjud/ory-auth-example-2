a823f60c4936214b9ef881616119c75271577d9a
sjud <64875465+sjud@users.noreply.github.com>
2024-03-30 16:03:02 -0400

    recovery -> settings page flow with test

---
+++ app/src/auth/settings.rs
@@ -0,0 +1,28 @@
+use super::*;
+/*
+ 
+*/
+#[component]
+pub fn SettingsPage() -> impl IntoView{
+    view!{
+
+    }
+}
+
+/*
+#[tracing::instrument]
+#[server]
+pub async fn reset_password_from_recovery_flow(flow_id:String) -> Result<(),ServerFnError> {
+    Ok(())
+}
+
+#[component]
+pub fn password_reset() -> impl IntoView {
+    if let Some(flow_id) = use_query_map().get_untracked().get("flow").cloned() {
+
+    }
+    view!{
+        
+    }
+}
+ */
